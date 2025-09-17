ef4640982a741bf493cdd1ee74c8684ccd217edf
Erfan-0 <elowix0@gmail.com>
2025-09-17 17:54:55 +0330

    #ed views.py(home and signup Class)

--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.contrib.auth.forms import UserChangeForm, AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, authenticate, logout as auth_logout
 from django.shortcuts import render, redirect
 from django.contrib import messages
@@ -15,21 +15,22 @@ from .forms import CommentForm
 
 def home(request): 
     posts = Post.objects.all().order_by('-created_at') # hame post ha ro migigre az Post va be tartin jadid tarin ha ro neshoon mide".order_by('-created_at')"
-    return render(request, 'posts/post_list.html', {'posts':posts}) 
-    
+    return render(request, 'posts/post_list.html', {'posts':posts}) # post haei ke gerefte shodan, tooye file html neshooon dade mishan. {'posts':posts} baraye iine ke ma betoonim post ha ro tooye file html tarif konim. mesle (post.title)
 
+# GET: safhe website be daste user reside va oon ro dide vali hanooz chizi ersal nakarde
+# POST : vaghti ke form ersal shode (masalan dokme signup ro zade va form ro ersal karde)
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
+    if request.method == 'POST': # age user dokme signup ro zad(form ersal karde bood(form zamani ejra mishe ke user feild haye marboot be signup kardan ro dorost por karde bashe va taeid shode bashe)) iin etefagh miyofte:
+        form = UserCreationForm(request.POST) # iin form ma hast. UserCrationForm form amade khode django hast ke chiz haei mesle username, password va... ro dar khodesh dare ma iinja tooye (request.POST) migi oon chiz haei ke dari mesle username, possword va... ro barabar gharar bede be oon chizi ke user vared karde. va hala dige form ma takmil mishe vali hanooz check nashode ke bebinim dorost por karde ya na. berim khate bad bebinim 
+        if form.is_valid(): # iinja check mishe aya etelaate form dorost va motabare ya na(masalan aya form kamel hast?, password ghavi hast? username tekrari nist?)
+            user = form.save() # age form motabar bashe user jadid ro save mikone tooye data base
+            login(request, user) # iin khat baes mishe ke niyaz nabashe user bad az singuo bere login kone. khodesh login ro barash anjam mide!
+            return redirect('home') # bad az hame iin kar ha. iin khat az code, user ro be home hedayat mikone
         else:
-            messages.error(request, "user is invalid")
-    else:
-        form = UserCreationForm()
-    return render(request, 'users/signup.html', {'form':form})
+            messages.error(request, "user is invalid") # error dar soorate invalid boodane user
+    else: # yani dar soorati ke request.method == 'GET' bashe(GET hame ke goftam. user safhe ro bebine tooye GET hast. chizi ersal kone mishe POST)
+        form = UserCreationForm() # age user hanooz form ro nafrestade(request.method karbar barabar ba POST nabashe) miyad form khali ijad mikone ke user poresh kone va singup bokone
+    return render(request, 'users/signup.html', {'form':form}) # che form valid bashe che invalid, signup.html ro neshoon mide ke gahleb ha namayesh dade beshan va user form ha ro bebine
 
 
 def login_page(request):
